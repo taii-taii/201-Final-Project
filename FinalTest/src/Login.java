165ac689b80326479d556260a7b0491ff2ae7f2b
Yash Chandak <ychandak@usc.edu>
2020-04-09 20:21:54 -0500

    first commit

--- FinalTest/src/Login.java
+++ FinalTest/src/Login.java
@@ -1,3 +1,7 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -35,17 +39,17 @@ public class Login {
 			System.out.println("User already exists.");
 			return;
 		}
-		st.executeUpdate("INSERT INTO UserInfo VALUES ('" + username +"','" +password + "')");
+		st.executeUpdate("INSERT INTO UserInfo VALUES ('" + username + "','" + password + "')");
 	}
 	
-	public boolean CheckPassword(String username,String password) throws SQLException{
-		if(VerifyUser(username)){
+	public boolean CheckPassword(String username, String password) throws SQLException, NoSuchAlgorithmException{
+		if(VerifyUser(username)) {
 			ps = conn.prepareStatement("SELECT * FROM UserInfo WHERE username='" + username + "'");
 			rs = ps.executeQuery();
 			rs.next();
-			if(rs.getString("password").equals(password)){
-				return true;
-			}
+			String[] saltAndHash = rs.getString("password").split("\\$");
+			String hashed = hash(password, saltAndHash[0].getBytes());
+			return hashed == saltAndHash[1];
 		}
 		return false;
 	}
@@ -53,19 +57,35 @@ public class Login {
 	public boolean VerifyUser(String username) throws SQLException{				
 		ps = conn.prepareStatement("SELECT EXISTS(SELECT * FROM UserInfo WHERE username='" + username +"')");
 		rs = ps.executeQuery();
-
 		rs.next();
-		if(rs.getBoolean(1)){
+		if(rs.getBoolean(1)) {
 			return true;
 		}
 		return false;
 	}
 	
-	public static void main (String[] args) {
+	public static String hash(String password, byte[] salt) throws NoSuchAlgorithmException {
+		if (password == null || password.length() == 0) {
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+		}
+		MessageDigest md = null;
+		md = MessageDigest.getInstance("SHA-512");
+		md.update(salt);
+		return new String(md.digest(password.getBytes(StandardCharsets.UTF_8)));
+	}
+	
+	public static String encryptPassword(String password) throws NoSuchAlgorithmException {
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[16];
+		random.nextBytes(salt);
+        return salt + "$" + hash(password, salt);
+    }
+	
+	public static void main(String[] args) {
 		String username;
 		String password;
 		
-		//Get info
+		// Get info
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Please enter your username: ");
 		username = scanner.nextLine();
@@ -74,18 +94,17 @@ public class Login {
 		System.out.println();
 		
 		Login login = new Login();
-		
 		Integer userChoice = null;
 		
-		//Execute
-		try{
+		// Execute
+		try {
 			System.out.print("Enter choice: \n1) Login\n2) Reset password\n3) Create new user\n");
 			userChoice = scanner.nextInt();
 			System.out.println();
 			
-			switch(userChoice){
+			switch(userChoice) {
 			case 1:
-				if(login.CheckPassword(username,password)){
+				if(login.CheckPassword(username, password)) {
 					System.out.println("Successfully logged in.");
 				} else {
 					System.out.println("Username or password do not match.");
@@ -93,18 +112,39 @@ public class Login {
 				System.out.println();
 				break;
 			case 2:
+				// Read password
 				System.out.print("Enter in a new password: ");
 				password = scanner.nextLine();
 				
-				login.ResetPassword(username,password);
+				// Encrypt
+				try {
+					password = encryptPassword(password);
+				} catch (IllegalArgumentException e) {
+					System.out.print("Invalid password");
+				} catch (NoSuchAlgorithmException e) {
+					System.out.println("NoSuchAlgorithmException: " + e.getMessage());
+				}
+				
+				// Reset
+				login.ResetPassword(username, password);
 				System.out.println();
 				break;
 			case 3:
+				// Encrypt
+				try {
+					password = encryptPassword(password);
+				} catch (IllegalArgumentException e) {
+					System.out.print("Invalid password");
+				} catch (NoSuchAlgorithmException e) {
+					System.out.println("NoSuchAlgorithmException: " + e.getMessage());
+				}
 				login.CreateUser(username, password);
 				break;
 			}
-		} catch(SQLException sqle){
-			System.out.println ("SQLException: " + sqle.getMessage());
+		} catch(SQLException sqle) {
+			System.out.println("SQLException: " + sqle.getMessage());
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("NoSuchAlgorithmException: " + e.getMessage());
 		}
 		
 		//Close resources
