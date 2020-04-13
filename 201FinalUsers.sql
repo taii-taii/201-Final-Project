9f6f99d6038f7c34d633de667be8abc6a5a13b7b
Tyler Amano-Smerling <amanosme@usc.edu>
2020-04-13 11:10:40 -0700

    Add files via upload

--- 201FinalUsers.sql
+++ 201FinalUsers.sql
@@ -1,6 +1,15 @@
 USE 201Final;
+
 DROP TABLE IF EXISTS UserInfo;
+DROP TABLE IF EXISTS DocumentManager;
+
 CREATE TABLE UserInfo(
-    username VARCHAR(12) NOT NULL,
+    username VARCHAR(12) PRIMARY KEY,
     password  VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE DocumentManager(
+	documentID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    docName VARCHAR(20) NOT NULL,
+    docContent MEDIUMBLOB
 );
